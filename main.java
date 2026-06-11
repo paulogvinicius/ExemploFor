397ff892e2b5d52282c4d6a9c043c790fba1821e
Paulo Vinícius <129632438+paulogvinicius@users.noreply.github.com>
2026-06-10 23:15:22 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        
+        // O loop começa em 1, vai até 5, e aumenta de 1 em 1 a cada rodada
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Contagem: " + i);
+        }
+        
+        System.out.println("Fim do loop!");
+    }
+}
